64b5aea48d3a4c297690ede61bf361a8e85a70c1
Victor Mesquini <mesquini@LIVE.COM>
2019-10-09 17:53:50 -0300

    create-frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 
 const app = express()
@@ -9,6 +10,8 @@ mongoose.connect('mongodb+srv://aircnc:aircnc@cluster0-2wxw0.mongodb.net/aircnc?
     useNewUrlParser : true,
     useUnifiedTopology: true
 })
+
+app.use(cors())
 app.use(express.json())
 
 app.use(routes)
